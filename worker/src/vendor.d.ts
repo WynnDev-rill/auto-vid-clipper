61210f3f797ede79ed4293747c3b6f36e9d33981
Wynn Dev <indonesiafilmku@gmail.com>
2026-08-15 19:29:23 +0700

    chore(worker): remove obsolete wavefile typings

--- worker/src/vendor.d.ts
+++
@@ -1,8 +0,0 @@
-declare module "wavefile" {
-  export class WaveFile {
-    constructor(data?: Buffer | Uint8Array);
-    toBitDepth(depth: string): void;
-    toSampleRate(rate: number): void;
-    getSamples(interleaved?: boolean, type?: string): Float32Array | Float32Array[];
-  }
-}
